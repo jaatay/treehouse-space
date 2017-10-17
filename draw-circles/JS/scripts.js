d1f46c21152b141224b4bc3a345ecb1fc740d4ae
Jesse Atay <jesse.atay@gmail.com>
2017-10-17 13:04:12 -0700

    new sub-project

--- draw-circles/JS/scripts.js
+++ draw-circles/JS/scripts.js
@@ -8,10 +8,14 @@ var testCounter = 0;
 function question (){
 var recordPress = prompt("How many circles do you want?");
 
+function randomColor (){
+  return Math.floor(Math.random() * 256 );
+}
+
   for (i = 0; i < parseInt(recordPress); i++) {
-  red = Math.floor(Math.random() * 256 );
-  green = Math.floor(Math.random() * 256 );
-  blue = Math.floor(Math.random() * 256 );
+  red = randomColor();
+  green = randomColor();
+  blue = randomColor();
   rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
   html = '<div style="background-color:' + rgbColor + '"></div>';
 
